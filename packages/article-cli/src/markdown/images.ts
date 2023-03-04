b64edc1c5fb951ebd2ad22d8a1675fd6ed5a49d8
Max Rohde <maxrohde.public@gmail.com>
2023-03-05 09:59:54 +1100

    Move static resources from old blogs to legacy CDN (#32)
    * Moving files to legacy cdn
    
    * Restoring attachements

--- packages/article-cli/src/markdown/images.ts
+++ packages/article-cli/src/markdown/images.ts
@@ -5,11 +5,15 @@ export function fixCoverImageLink(link: string): string {
 export function fixContentLinks(markdown: string): string {
   let res = markdown.replaceAll(
     /https:\/\/spearoflight.files.wordpress.com\//g,
-    '/_goldstack/static/img/spearoflight-media/'
+    'https://legacy-cdn.pureleap.com/static/spearoflight-media/'
+  );
+  res = res.replaceAll(
+    /https:\/\/nexnet.files.wordpress.com\//g,
+    'https://legacy-cdn.pureleap.com/static/maxrohde.com-media/'
   );
   res = res.replaceAll(
     /https:\/\/shalveena.files.wordpress.com\//g,
-    '/_goldstack/static/img/shalveena.com-media/'
+    'https://legacy-cdn.pureleap.com/static/shalveena.com-media/'
   );
   return res;
 }
