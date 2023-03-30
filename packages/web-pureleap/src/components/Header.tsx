a5a8e57c6205149166fbf16fc81295e6e08a4535
Max Rohde <1448524+mxro@users.noreply.github.com>
2023-03-31 07:51:50 +1100

    Creating Header and Hero section

---
+++ packages/web-pureleap/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export function Header(): JSX.Element {
+  return (
+    <header className="fixed top-0 w-full flex items-center justify-between px-6 py-4 bg-white bg-opacity-0 backdrop-filter backdrop-blur-md z-10 transition-all duration-500 ease-in-out">
+      <div className="text-white font-bold text-xl">Pureleap</div>
+      <nav className="flex items-center justify-end">
+        <a href="#" className="text-white mx-4 hover:text-gray-300">
+          About
+        </a>
+        <a href="#" className="text-white mx-4 hover:text-gray-300">
+          Blog
+        </a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
--- packages/web-pureleap/src/routes/$index.tsx
+++ packages/web-pureleap/src/routes/$index.tsx
@@ -2,38 +2,25 @@ import React, { useState } from 'react';
 import { SSRHandler } from '@goldstack/template-ssr';
 
 import { renderPage, hydrate } from './../render';
-import Panel from './../components/Panel';
 import styles from './$index.module.css';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
 
-const Index = (props: { message: string }): JSX.Element => {
-  const [clicked, setClicked] = useState(false);
+const Index = (): JSX.Element => {
   return (
     <>
-      <div
-        onClick={() => {
-          alert('hi');
-          setClicked(true);
-          throw new Error('Havent seen this');
-        }}
-        className={`${styles.message}`}
-      >
-        {props.message}
-      </div>
-      {clicked && <Panel />}
+      <Header></Header>
+      <Hero></Hero>
     </>
   );
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const handler: SSRHandler = async (event, context) => {
-  const message = 'Hi there';
-
   return renderPage({
     component: Index,
-    appendToHead: '<title>SSR Template</title>',
-    properties: {
-      message,
-    },
+    appendToHead: '<title>Pureleap - Live simply, Serve greatly</title>',
+    properties: {},
     entryPoint: __filename,
     event,
   });
