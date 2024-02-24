30c4df8e300110f030cbb0bd11e7ceeeef4d0c81
Max Rohde <1448524+mxro@users.noreply.github.com>
2024-02-25 07:32:33 +1100

    Fixing uuid issue

--- packages/web-maxrohde-com/jest.config.ui.js
+++ packages/web-maxrohde-com/jest.config.ui.js
@@ -5,6 +5,10 @@ module.exports = {
   ...base,
   // only run uispec tests since SSR tests do not run in jsdom environment
   testRegex: '\\.(uispec)\\.ts[x]?$',
+  // since not resolved yet https://github.com/aws/aws-sdk-js-v3/issues/3964
+  moduleNameMapper: {
+    uuid: require.resolve('uuid'),
+  },
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json',
